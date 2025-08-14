1ae8ff09b86eb443478cfb9d01fb07c41002cc4a
dharaneeshponnuvel <dharaneeshp56@gmail.com>
2025-08-14 20:38:29 +0530

    Fix TypeScript errors for req.session and AuthRequest types

--- src/supabase/supabaseClient.js
+++ src/supabase/supabaseClient.js
@@ -1,7 +1,6 @@
-// src/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://iifrqchsujoqptmnbzwb.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpZnJxY2hzdWpvcXB0bW5iendiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ0OTMyMzUsImV4cCI6MjA3MDA2OTIzNX0.j1WF7kZ_Y_qrVVbKytOUfCshs91S4XVePtKWq_FE2MM';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
